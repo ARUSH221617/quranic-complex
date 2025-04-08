a6a4a869d2646f2b30848f00ebbcc5b1b7fc3929
ARUSH <arush221617@gmail.com>
2025-04-08 09:48:43 +0330

    feat(layout): create RootLayout component and implement sidebar structure

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import SessionWrapper from "@/components/SessionWrapper";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+
+export const metadata: Metadata = {
+  title: "Khorramshahr Quranic Complex",
+  description: "Educational center for Quranic studies",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang={"en"} dir={"ltr"}>
+      <body>
+        <SessionWrapper>
+          <main>
+            <SidebarProvider>
+              <AppSidebar variant="inset" />
+              <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                  <div className="@container/main flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                      {children}
+                    </div>
+                  </div>
+                </div>
+              </SidebarInset>
+            </SidebarProvider>
+          </main>
+        </SessionWrapper>
+      </body>
+    </html>
+  );
+}
